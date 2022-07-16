a8dfd5aae34122b8990364062ed8ed39baf49bc4
giri-gcp01 <109426840+giri-gcp01@users.noreply.github.com>
2022-07-16 20:37:36 +0530

    Iniital Commint and source

---
+++ src/main/java/com/googlecloud/pubsub/subscriber/SubscribeUtils.java
@@ -0,0 +1,48 @@
+package com.googlecloud.pubsub.subscriber;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+import com.google.pubsub.v1.PubsubMessage;
+
+public class SubscribeUtils {
+	
+	public void main(String... args) throws Exception {
+	    // TODO(developer): Replace these variables before running the sample.
+	    String projectId = "cloud-foundry-354707";
+	    String subscriptionId = "pubsubtest-sub";
+
+	    subscribeAsyncExample(projectId, subscriptionId);
+	  }
+
+	  public void subscribeAsyncExample(String projectId, String subscriptionId) {
+	    ProjectSubscriptionName subscriptionName =
+	        ProjectSubscriptionName.of(projectId, subscriptionId);
+
+	    // Instantiate an asynchronous message receiver.
+	    MessageReceiver receiver =
+	        (PubsubMessage message, AckReplyConsumer consumer) -> {
+	          // Handle incoming message, then ack the received message.
+	          System.out.println("Data: " + message.getData().toStringUtf8());
+	          consumer.ack();
+	        };
+
+	    Subscriber subscriber = null;
+	    try {
+	      subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+	      // Start the subscriber.
+	      subscriber.startAsync().awaitRunning();
+	      System.out.printf("Listening for messages on %s:\n", subscriptionName.toString());
+	      // Allow the subscriber to run for 30s unless an unrecoverable error occurs.
+	      subscriber.awaitTerminated(300000, TimeUnit.SECONDS);
+	    } catch (TimeoutException timeoutException) {
+	      // Shut down the subscriber after 30s. Stop receiving messages.
+	      subscriber.stopAsync();
+	    }
+	  }
+
+}
---
+++ src/test/java/com/googlecloud/pubsub/subscriber/PubsubSubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googlecloud.pubsub.subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class PubsubSubscriberApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
